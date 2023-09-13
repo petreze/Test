5b1d52cb3f48290021f3b4723e40022cf894fc4c
Petar Tonev <petar.tonev@limechain.tech>
2023-09-14 00:36:34 +0300

    init
    Signed-off-by: Petar Tonev <petar.tonev@limechain.tech>

--- src/App.js
+++ src/App.js
@@ -32,19 +32,7 @@ import {
 
 function App() {
 async function main() {
-  /* const myAccountId = AccountId.fromString("0.0.1228");
-  const operatorId = AccountId.fromString("0.0.1238");
-  const operatorKey = PrivateKey.fromString(
-      "3030020100300706052b8104000a04220420a1950bc0588663781a4159963ec83b3a7ceb18547fff6d37294a1eeea02bcf5c"
-  ); */
-  const myPrivateKey = PrivateKey.fromString("302e020100300506032b6570042204203981726b8bda6cd74bad38c1dd14c13fb94b42476477f85ba7987e2075f860ab");
-  const testmyAccountId = AccountId.fromString("0.0.1420842");
-  const testoperatorId = AccountId.fromString("0.0.2879225");
-  const testoperatorKey = PrivateKey.fromString("302e020100300506032b6570042204205014313e2a6c63c6c16425c16b62c46c72ba49e11687b7f5ee3ed3f6682340f4");
-
   const myAccountId = AccountId.fromString("0.0.425551");
-  /* const operatorId = AccountId.fromString("0.0.425553");
-  const operatorKey = PrivateKey.fromString("3030020100300706052b8104000a04220420c57eda61e35954d24130dbdba089be3cfd386d336f5e7fe56f11d4b4990d9bfc"); */
 
   const operatorId = AccountId.fromString("0.0.950")
   const operatorKey = PrivateKey.fromStringED25519("302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137")
